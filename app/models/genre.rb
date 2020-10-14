c674c0e6b7f70cdefa084c4a947f45e54722baac
Ariel I <arielibarra39@gmail.com>
2020-10-14 01:12:21 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -12,7 +12,7 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    self.artists(:name)
+    self.find_by(artists: "name")
 
     # return an array of strings containing every musician's name
   end
